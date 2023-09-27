b4e256ef9a18a9808f4d54219f4c9ca56421a825
akio0911 <akio0911@gmail.com>
2023-09-27 22:29:53 +0900

    feedback

--- Kadai1/ViewController.swift
+++ Kadai1/ViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 class ViewController: UIViewController {
 
     // テキストフィールドの配置、入力文字の取得
-    @IBOutlet weak var textField1: UITextField!
-    @IBOutlet weak var textField2: UITextField!
-    @IBOutlet weak var textField3: UITextField!
-    @IBOutlet weak var textField4: UITextField!
-    @IBOutlet weak var textField5: UITextField!
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var textField3: UITextField!
+    @IBOutlet private weak var textField4: UITextField!
+    @IBOutlet private weak var textField5: UITextField!
+
+    // ラベルの配置
+    @IBOutlet private weak var sumLabel: UILabel!
 
     // ボタンの配置、テキストフィールドの合計をラベルに送る
-    @IBAction func sumButton(_ sender: Any) {
+    @IBAction private func sumButton(_ sender: Any) {
         // テキストフィールドの値を数値に変換
         let number1 = Int(textField1.text ?? "") ?? 0
         let number2 = Int(textField2.text ?? "") ?? 0
@@ -35,14 +38,4 @@ class ViewController: UIViewController {
         // 合計を文字列化して、ラベルに表示
         sumLabel.text = "\(result)"
     }
-
-    // ラベルの配置
-    @IBOutlet weak var sumLabel: UILabel!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
 }
-
